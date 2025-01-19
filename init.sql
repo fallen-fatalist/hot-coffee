cd03c2f4bb0929d0023688bddeeda55e329beb62
Murat Kairbek <m_kairbek@outlook.com>
2025-01-19 23:02:41 +0500

    removed foreign keys to status and units; fixed syntax errors;

--- init.sql
+++ init.sql
@@ -1,23 +1,6 @@
 -- CREATE TYPE status_type AS ENUM ('accepted', 'rejected');
 -- CREATE TABLE unit_type AS ENUM ('shots', 'ml', 'g');
 
------------------
--- Orders part --
------------------
-
-CREATE TABLE orders(
-    order_id SERIAL PRIMARY KEY,
-    customer_id INTEGER NOT NULL ,
-    status_id INTEGER NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customers (customer_id),
-    FOREIGN KEY (status_id) REFERENCES statuses (status_id)
-);
-
-CREATE TABLE statuses(
-    status_id SERIAL PRIMARY KEY,
-    name TEXT
-);
 
 CREATE TABLE customers(
     customer_id SERIAL PRIMARY KEY,
@@ -26,63 +9,67 @@ CREATE TABLE customers(
     phone VARCHAR(20) CHECK (phone ~ '^\+?[0-9\-()\s]{7,20}$')
 );
 
+CREATE TABLE orders(
+    order_id SERIAL PRIMARY KEY,
+    customer_id INTEGER NOT NULL,
+    status TEXT,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    FOREIGN KEY (customer_id) REFERENCES customers (customer_id)
+);
+
+CREATE TABLE statuses(
+    status_id SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+
 CREATE TABLE order_status_history(
     order_id INTEGER NOT NULL,
-    past_status INTEGER NOT NULL,
-    new_status INTEGER NOT NULL,
-    changed_at TIMESTAMPTZ NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders (order_id),
-    FOREIGN KEY (past_status) REFERENCES statuses (status_id),
-    FOREIGN KEY (new_status) REFERENCES STATUSES (status_id)
+    past_status TEXT NOT NULL,
+    new_status TEXT NOT NULL,
+    changed_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    FOREIGN KEY (order_id) REFERENCES orders (order_id) 
 );
 
-CREATE TABLE order_items(
-    menu_item_id INTEGER NOT NULL, 
-    order_id INTEGER NOT NULL,
-    quantity DECIMAL(10, 2) NOT NULL,
-    customization_info TEXT NOT NULL,
-    FOREIGN KEY (menu_item_id) REFERENCES menu_items (menu_item_id),
-    FOREIGN KEY (order_id) REFERENCES orders (order_id)
-);
-
----------------
--- Menu part --
----------------
 
 CREATE TABLE menu_items(
     menu_item_id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     description TEXT NOT NULL, 
-    price INT NOT NULL,
-    created_at TIMESTAMPTZ
+    price INT NOT NULL
+);
+
+CREATE TABLE order_items(
+    menu_item_id INTEGER NOT NULL, 
+    order_id INTEGER NOT NULL,
+    quantity DECIMAL(10, 5) NOT NULL,
+    customization_info TEXT NOT NULL,
+    FOREIGN KEY (menu_item_id) REFERENCES menu_items (menu_item_id),
+    FOREIGN KEY (order_id) REFERENCES orders (order_id)
 );
 
 CREATE TABLE price_history(
     menu_item_id INTEGER NOT NULL,
     price_difference INT NOT NULL,
-    changed_at TIMESTAMPTZ NOT NULL,
+    changed_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     FOREIGN KEY (menu_item_id) REFERENCES menu_items (menu_item_id)
+);
+
+
+
+CREATE TABLE inventory(
+    inventory_item_id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    quantity DECIMAL(10,5) NOT NULL,
+    unit VARCHAR(20)
 );
 
 CREATE TABLE menu_items_ingredients(
     menu_item_id INTEGER NOT NULL,
     inventory_item_id INTEGER NOT NULL,
-    quantity DECIMAL(10, 2) NOT NULL,
+    quantity DECIMAL(10, 5) NOT NULL,
     FOREIGN KEY (menu_item_id) REFERENCES menu_items (menu_item_id),
     FOREIGN KEY (inventory_item_id) REFERENCES inventory (inventory_item_id)
-);
-
---------------------
--- Inventory part --
---------------------
-
-CREATE TABLE inventory(
-    inventory_item_id SERIAL PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
-    quantity DECIMAL(10,2) NOT NULL,
-    unit_id INT NOT NULL,
-    created_at TIMESTAMPTZ,
-    FOREIGN KEY (unit_id) REFERENCES units (unit_id)
 );
 
 CREATE TABLE units(
@@ -92,7 +79,7 @@ CREATE TABLE units(
 
 CREATE TABLE inventory_transactions(
     inventory_item_id SERIAL PRIMARY KEY,
-    transaction_quantity DECIMAL(10, 2) NOT NULL,
-    changed_at TIMESTAMPTZ NOT NULL,
+    transaction_quantity DECIMAL(10, 5) NOT NULL,
+    changed_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     FOREIGN KEY (inventory_item_id) REFERENCES inventory (inventory_item_id)
 );
