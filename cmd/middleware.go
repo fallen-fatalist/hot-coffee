6d5564793f65c8204bfddd62f086cfc82368584e
Murat Kairbek <m_kairbek@outlook.com>
2025-02-05 14:26:49 +0500

    changed the name of middleware;

--- cmd/middleware.go
+++ cmd/middleware.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-func loggingMiddleware(next http.Handler) http.Handler {
+func requestLoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		slog.Info(fmt.Sprintf("%s request with URL: %s", r.Method, r.URL.String()))
 		next.ServeHTTP(w, r)
