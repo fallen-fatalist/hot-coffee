aeb01119e34b867ccac8a5cf2673a8d86b822839
Murat Kairbek <m_kairbek@outlook.com>
2025-02-06 13:20:25 +0500

    Added wrapper function Is for native errors.Is; Added custom error for non integer IDs;

--- internal/core/errors/errors.go
+++ internal/core/errors/errors.go
@@ -5,6 +5,18 @@ import (
 	"fmt"
 )
 
+func New(text string) error {
+	return errors.New(text)
+}
+
+func As(err error, target interface{}) bool {
+	return errors.As(err, target)
+}
+
+func Is(err error, target error) bool {
+	return errors.Is(err, target)
+}
+
 // TODO: Add more error types
 // TODO: Add more context to errors
 
@@ -22,10 +34,15 @@ func (err *ErrInsufficientIngredient) Error() string {
 	return fmt.Sprintf("Insufficient: %s ingredient id", err.insufficientIngredientID)
 }
 
-func New(text string) error {
-	return errors.New(text)
+type ErrNonIntegerID struct {
+	entity     string
+	providedID string
 }
 
-func As(err error, target interface{}) bool {
-	return errors.As(err, target)
+func NewErrNonIntegerID(entity, providedID string) *ErrNonIntegerID {
+	return &ErrNonIntegerID{entity, providedID}
+}
+
+func (err *ErrNonIntegerID) Error() string {
+	return fmt.Sprintf("%s id of %s is not integer", err.providedID, err.entity)
 }
