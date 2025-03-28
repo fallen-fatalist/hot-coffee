c33375784630fc3caad80d9b61589745506601cf
Murat Kairbek <m_kairbek@outlook.com>
2025-03-28 20:02:21 +0500

    renamed scripts; removed redundant statement

--- migrations/024_create_index_order_items.sql
+++ migrations/024_create_index_order_items.sql
@@ -1,4 +1,3 @@
 --order_items as oi idx
 CREATE INDEX oi_quantity_idx ON order_items (quantity);
 CREATE INDEX oi_orderid_idx ON order_items (order_id);
-CREATE INDEX oi_orderid_idx ON order_items (order_id);
